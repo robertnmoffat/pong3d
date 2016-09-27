6a9dd1b6c627a297d942b8c5af83e37fd13b25b8
robert moffat <robert moffat>
2016-09-27 12:53:13 -0700

    started adding score

---
+++ Assets/ScoreScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour {
+    Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+        scoreText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = "TEST";
+	}
+}
